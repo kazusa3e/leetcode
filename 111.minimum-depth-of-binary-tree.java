f807be645ae08f4f4c0dc4dd4e1ff6b603c9f5cd
kazusa <i@kazusa.me>
2024-04-05 18:31:09 +0800

    Fri Apr  5 18:31:09 CST 2024

---
+++ 111.minimum-depth-of-binary-tree.java
@@ -0,0 +1,51 @@
+/*
+ * @lc app=leetcode id=111 lang=java
+ *
+ * [111] Minimum Depth of Binary Tree
+ */
+
+// @lc code=start
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+class Solution {
+    public int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        Queue<TreeNode> queue = new ArrayDeque<>();
+        queue.add(root);
+        int depth = 0;
+        while (!queue.isEmpty()) {
+            depth += 1;
+            for (int sz = queue.size(); sz != 0; --sz) {
+                TreeNode node = queue.remove();
+                if (node.left == null && node.right == null) {
+                    return depth;
+                }
+                if (node.left != null)
+                    queue.add(node.left);
+                if (node.right != null)
+                    queue.add(node.right);
+            }
+        }
+        return depth;
+    }
+}
+// @lc code=end
---
+++ 117.populating-next-right-pointers-in-each-node-ii.java
@@ -0,0 +1,58 @@
+/*
+ * @lc app=leetcode id=117 lang=java
+ *
+ * [117] Populating Next Right Pointers in Each Node II
+ */
+
+// @lc code=start
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+class Solution {
+    public Node connect(Node root) {
+        Queue<Node> queue = new ArrayDeque<>();
+        if (root == null) {
+            return null;
+        }
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            Node prev = null;
+            for (int sz = queue.size(); sz != 0; --sz) {
+                Node curr = queue.remove();
+                if (curr.left != null)
+                    queue.add(curr.left);
+                if (curr.right != null)
+                    queue.add(curr.right);
+                if (prev != null) {
+                    prev.next = curr;
+                }
+                prev = curr;
+            }
+        }
+        return root;
+    }
+}
+// @lc code=end
