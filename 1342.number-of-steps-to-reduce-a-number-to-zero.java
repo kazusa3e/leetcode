a2add95187f980868029bb284e4ff8a89a28ad26
kazusa <i@kazusa.me>
2024-04-10 10:14:25 +0800

    Wed Apr 10 10:14:25 CST 2024

---
+++ 1342.number-of-steps-to-reduce-a-number-to-zero.java
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode id=1342 lang=java
+ *
+ * [1342] Number of Steps to Reduce a Number to Zero
+ */
+
+// @lc code=start
+class Solution {
+    public int numberOfSteps(int num) {
+        int steps = 0;
+        while (num != 0) {
+            steps += 1;
+            num = (num % 2 == 0) ? num / 2 : num - 1;
+        }
+        return steps;
+    }
+}
+// @lc code=end
+
