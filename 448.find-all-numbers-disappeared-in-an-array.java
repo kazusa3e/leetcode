a7854eb22ddb096450ec2f90024fb7626e62a41f
kazusa <i@kazusa.me>
2024-04-11 10:17:34 +0800

    Thu Apr 11 10:17:34 CST 2024

--- 448.find-all-numbers-disappeared-in-an-array.java
+++ 448.find-all-numbers-disappeared-in-an-array.java
@@ -10,21 +10,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 class Solution {
-    private void swap(int[] arr, int i, int j) {
-        int tmp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = tmp;
-    }
     public List<Integer> findDisappearedNumbers(int[] nums) {
         List<Integer> ret = new ArrayList<>();
         for (int ix = 0; ix != nums.length; ++ix) {
-            while (nums[ix] != ix + 1) {
-                if (nums[ix] == nums[nums[ix] - 1]) break;
-                swap(nums, ix, nums[ix] - 1);
+            int idx = Math.abs(nums[ix]) - 1;
+            if (nums[idx] > 0) {
+                nums[idx] = -nums[idx];
             }
         }
         for (int ix = 0; ix != nums.length; ++ix) {
-            if (nums[ix] != ix + 1) { ret.add(ix + 1); }
+            if (nums[ix] > 0) ret.add(ix + 1);
         }
         return ret;
     }
