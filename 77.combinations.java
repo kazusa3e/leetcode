753e5b5c5089b1a4c0101a2f6e8b90fee3484631
kazusa <i@kazusa.me>
2024-04-11 13:51:04 +0800

    Thu Apr 11 13:51:04 CST 2024

---
+++ 77.combinations.java
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode id=77 lang=java
+ *
+ * [77] Combinations
+ */
+
+// @lc code=start
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    private List<List<Integer>> result;
+    private List<Integer> path;
+    private void backtracking(int n, int k, int startIndex) {
+        if (path.size() == k) {
+            result.add(new ArrayList<>(path));
+            return;
+        }
+        for (int i = startIndex; i != n + 1; ++i) {
+            path.add(i);
+            backtracking(n, k, i + 1);
+            path.removeLast();
+        }
+    }
+    public List<List<Integer>> combine(int n, int k) {
+        result = new ArrayList<>();
+        path = new ArrayList<>();
+        backtracking(n, k, 1);
+        return result;
+    }
+}
+// @lc code=end
+
