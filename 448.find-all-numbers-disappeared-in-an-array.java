28787f58acfca8d43ea7a2b684f0b552b905aa55
kazusa <i@kazusa.me>
2024-04-11 10:07:06 +0800

    Thu Apr 11 10:07:06 CST 2024

--- 448.find-all-numbers-disappeared-in-an-array.java
+++ 448.find-all-numbers-disappeared-in-an-array.java
@@ -7,25 +7,24 @@
 // @lc code=start
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 class Solution {
+    private void swap(int[] arr, int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
     public List<Integer> findDisappearedNumbers(int[] nums) {
         List<Integer> ret = new ArrayList<>();
-        Arrays.sort(nums);
-        for (int i = 1; i != nums[0]; ++i) {
-            ret.add(i);
-        }
-        for (int ix = 1; ix != nums.length; ++ix) {
-            if (nums[ix] - nums[ix - 1] > 1) {
-                for (int i = nums[ix - 1] + 1; i != nums[ix]; ++i) {
-                    ret.add(i);
-                }
+        for (int ix = 0; ix != nums.length; ++ix) {
+            while (nums[ix] != ix + 1) {
+                if (nums[ix] == nums[nums[ix] - 1]) break;
+                swap(nums, ix, nums[ix] - 1);
             }
         }
-        for (int i = nums[nums.length - 1] + 1; i <= nums.length; ++i) {
-            ret.add(i);
+        for (int ix = 0; ix != nums.length; ++ix) {
+            if (nums[ix] != ix + 1) { ret.add(ix + 1); }
         }
         return ret;
     }
