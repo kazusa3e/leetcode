56c6cbcfc183ccc74b7301098955c4060bb7270c
kazusa <i@kazusa.me>
2024-04-11 14:34:11 +0800

    Thu Apr 11 14:34:11 CST 2024

--- 17.letter-combinations-of-a-phone-number.java
+++ 17.letter-combinations-of-a-phone-number.java
@@ -12,7 +12,7 @@ import java.util.Map;
 
 class Solution {
     private List<String> results;
-    private List<Character> letters;
+    private StringBuilder letters;
     private Map<Character, List<Character>> map = Map.of(
             '2', List.of('a', 'b', 'c'),
             '3', List.of('d', 'e', 'f'),
@@ -28,23 +28,21 @@ class Solution {
             return new ArrayList<>();
         }
         results = new ArrayList<>();
-        letters = new ArrayList<>();
+        letters = new StringBuilder(digits.length());
         backtracking(digits.toCharArray(), 0);
         return results;
     }
 
     private void backtracking(char[] digits, int startIndex) {
-        if (letters.size() == digits.length) {
-            StringBuilder sb = new StringBuilder();
-            for (char ch : letters) sb.append(ch);
-            results.add(sb.toString());
+        if (letters.length() == digits.length) {
+            results.add(letters.toString());
             return;
         }
         List<Character> chars = map.get(digits[startIndex]);
         for (char ch : chars) {
-            letters.add(ch);
+            letters.append(ch);
             backtracking(digits, startIndex + 1);
-            letters.removeLast();
+            letters.deleteCharAt(letters.length() - 1);
         }
     }
 }
