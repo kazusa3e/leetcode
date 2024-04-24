16bab700826fd5ed3f309cf81ced84401fdadab3
kazusa <i@kazusa.me>
2024-04-24 21:56:05 +0800

    Wed Apr 24 21:56:05 CST 2024

--- 509.fibonacci-number.java
+++ 509.fibonacci-number.java
@@ -5,11 +5,21 @@
  */
 
 // @lc code=start
+
+import java.util.ArrayList;
+import java.util.List;
+
 class Solution {
     public int fib(int n) {
-        if (n == 0 || n == 1)
-            return n;
-        return fib(n - 1) + fib(n - 2);
+        List<Integer> table = new ArrayList<>();
+        table.add(0);
+        table.add(1);
+        int idx = 2;
+        while (idx <= n) {
+            table.add(table.get(idx - 1) + table.get(idx - 2));
+            idx += 1;
+        }
+        return table.get(n);
     }
 }
 // @lc code=end
