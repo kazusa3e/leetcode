bf15edca7059c59ceda09f48bf03685693aa482b
kazusa <i@kazusa.me>
2024-05-15 09:20:42 +0800

    Wed May 15 09:20:42 CST 2024

--- 112.path-sum.cpp
+++ 112.path-sum.cpp
@@ -16,21 +16,38 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+#include <stack>
+
+using namespace std;
+
 class Solution {
 public:
     bool hasPathSum(TreeNode* root, int targetSum) {
         if (root == nullptr) return false;
-        return hasPathSum(root, targetSum, 0);
-    }
-    bool hasPathSum(TreeNode *root, int targetSum, int preSum) {
-        if (root->left == nullptr && root->right == nullptr) {
-            if (root->val + preSum == targetSum) return true;
-        }
-        if (root->left != nullptr) {
-            if (hasPathSum(root->left, targetSum, preSum + root->val)) return true;
-        }
-        if (root->right != nullptr) {
-            if (hasPathSum(root->right, targetSum, preSum + root->val)) return true;
+        stack<TreeNode *> st;
+        stack<int> presum;
+        st.push(root);
+        presum.push(0);
+        while (!st.empty()) {
+            TreeNode *node = st.top(); st.pop();
+            int sum = presum.top(); presum.pop();
+            if (node != nullptr) {
+                if (node->right != nullptr) {
+                    st.push(node->right);
+                    presum.push(sum + node->val);
+                }
+                if (node->left != nullptr) {
+                    st.push(node->left);
+                    presum.push(sum + node->val);
+                }
+                st.push(node); st.push(nullptr); presum.push(sum);
+            } else {
+                node = st.top(); st.pop();
+                if (node->left == nullptr && node->right == nullptr) {
+                    if (sum + node->val == targetSum) return true;
+                }
+            }
         }
         return false;
     }
