c52949525bd21539a052b5255aa249fa1853d3d7
kazusa <i@kazusa.me>
2024-06-06 13:00:18 +0800

    Thu Jun  6 01:00:18 PM CST 2024

--- 491.non-decreasing-subsequences.cpp
+++ 491.non-decreasing-subsequences.cpp
@@ -14,25 +14,22 @@ using namespace std;
 class Solution {
 private:
     vector<int> nums_;
-    vector<vector<int>> results_;
     vector<int> bag_;
-
-    void backtracking(vector<int>::iterator pos) {
+    vector<vector<int>> results_;
+    void backtracking(vector<int>::iterator begin) {
         if (bag_.size() >= 2) {
             results_.push_back(bag_);
         }
         unordered_set<int> seen;
-        for (auto iter = pos; iter != nums_.end(); ++iter) {
-            if (auto f = seen.find(*iter); f != seen.end()) continue;
+        for (auto iter = begin; iter != nums_.end(); ++iter) {
+            if (auto pos = seen.find(*iter); pos != seen.end()) continue;
             seen.insert(*iter);
-            if (bag_.empty() || *iter >= bag_.back()) {
-                bag_.push_back(*iter);
-                backtracking(iter + 1);
-                bag_.pop_back();
-            }
+            if (!bag_.empty() && *iter < bag_.back()) continue;
+            bag_.push_back(*iter);
+            backtracking(iter + 1);
+            bag_.pop_back();
         }
     }
-
 public:
     vector<vector<int>> findSubsequences(vector<int>& nums) {
         nums_ = nums;
