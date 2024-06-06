fc6e4d89926fa871baab19bac42460efb6df7eba
kazusa <i@kazusa.me>
2024-06-06 13:08:43 +0800

    Thu Jun  6 01:08:43 PM CST 2024

--- 46.permutations.cpp
+++ 46.permutations.cpp
@@ -12,22 +12,26 @@ using namespace std;
 
 class Solution {
 private:
+    vector<int> nums_;
+    vector<int> bag_;
     vector<vector<int>> results_;
-    void backtracking(vector<int> bag, vector<int> collections) {
-        if (collections.empty()) {
-            results_.push_back(bag);
+    void backtracking(vector<int> candidates) {
+        if (candidates.empty()) {
+            results_.push_back(bag_);
+            return;
         }
-        for (auto iter = collections.begin(); iter != collections.end(); ++iter) {
-            vector<int> b {bag}, c {collections};
-            b.push_back(*iter);
-            c.erase(c.begin() + (iter - collections.begin()));
-            backtracking(b, c);
+        for (auto iter = candidates.begin(); iter != candidates.end(); ++iter) {
+            bag_.push_back(*iter);
+            vector<int> c {candidates};
+            c.erase(c.begin() + distance(candidates.begin(), iter));
+            backtracking(c);
+            bag_.pop_back();
         }
     }
-
 public:
     vector<vector<int>> permute(vector<int>& nums) {
-        backtracking({}, nums);
+        nums_ = nums;
+        backtracking(nums_);
         return results_;
     }
 };
