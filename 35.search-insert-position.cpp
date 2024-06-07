78bbaaed93aab3395411218659db9142ba032379
kazusa <i@kazusa.me>
2024-06-07 10:56:16 +0800

    Fri Jun  7 10:56:16 AM CST 2024

--- 35.search-insert-position.cpp
+++ 35.search-insert-position.cpp
@@ -12,25 +12,7 @@ using namespace std;
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        if (nums.size() == 0) {
-            return (target <= nums.at(0)) ? 0 : 1;
-        }
-        if (target > nums.back()) {
-            return nums.size();
-        }
-        using size_type = decltype(nums.size());
-        size_type left = 0, right = nums.size();
-        while (left < right) {
-            size_type mid = left + ((right - left) >> 1);
-            if (target < nums.at(mid)) {
-                right = mid;
-            } else if (target > nums.at(mid)) {
-                left = mid + 1;
-            } else {
-                return mid;
-            }
-        }
-        return left;
+        return distance(nums.begin(), lower_bound(nums.begin(), nums.end(), target));
     }
 };
 // @lc code=end
