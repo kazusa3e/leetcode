60e430d6f7b009b4da46abf74fe4f46203997529
kazusa <i@kazusa.me>
2024-06-07 10:02:49 +0800

    Fri Jun  7 10:02:49 AM CST 2024

--- 941.valid-mountain-array.cpp
+++ 941.valid-mountain-array.cpp
@@ -14,23 +14,11 @@ class Solution {
 public:
     bool validMountainArray(vector<int>& arr) {
         if (arr.size() < 3) return false;
-        enum class STATE {INCREASING, DECREASING};
-        STATE state = STATE::INCREASING;
-        for (int ix = 1; ix != arr.size(); ++ix) {
-            if (state == STATE::INCREASING) {
-                if (arr[ix] > arr[ix - 1]) continue;
-                else if (arr[ix] < arr[ix - 1]) {
-                    if (ix == 1) return false;
-                    state = STATE::DECREASING;
-                } else {
-                    return false;
-                }
-            } else {
-                if (arr[ix] < arr[ix - 1]) continue;
-                else return false;
-            }
-        }
-        return (state == STATE::DECREASING);
+        int left = 0, right = arr.size() - 1;
+        while (left + 1 != arr.size() && arr[left + 1] > arr[left]) left += 1;
+        while (right - 1 != 0 && arr[right - 1] > arr[right]) right -= 1;
+        if (left == 0 || right == arr.size() - 1) return false;
+        return (left == right);
     }
 };
 // @lc code=end
