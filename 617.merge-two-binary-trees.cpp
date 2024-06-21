2014603103baf61cf80382ee3815a8095acdb5cc
kazusa <i@kazusa.me>
2024-06-21 11:44:46 +0800

    Fri Jun 21 11:44:46 AM CST 2024

--- 617.merge-two-binary-trees.cpp
+++ 617.merge-two-binary-trees.cpp
@@ -16,14 +16,34 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+#include <queue>
+using namespace std;
+
 class Solution {
 public:
     TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) {
         if (root1 == nullptr) return root2;
         if (root2 == nullptr) return root1;
-        root1->val += root2->val;
-        root1->left = mergeTrees(root1->left, root2->left);
-        root1->right = mergeTrees(root1->right, root2->right);
+        queue<TreeNode *> qu;
+        qu.push(root1); qu.push(root2);
+        while (!qu.empty()) {
+            TreeNode *n1 = qu.front(); qu.pop();
+            TreeNode *n2 = qu.front(); qu.pop();
+            n1->val += n2->val;
+            if (n1->left != nullptr && n2->left != nullptr) {
+                qu.push(n1->left); qu.push(n2->left);
+            }
+            if (n1->right != nullptr && n2->right != nullptr) {
+                qu.push(n1->right); qu.push(n2->right);
+            }
+            if (n1->left == nullptr && n2->left != nullptr) {
+                n1->left = n2->left;
+            }
+            if (n1->right == nullptr && n2->right != nullptr) {
+                n1->right = n2->right;
+            }
+        }
         return root1;
     }
 };
