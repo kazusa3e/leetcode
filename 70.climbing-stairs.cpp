98d4c8b22b16536ed0ccbb0776a65241a234a2e9
kazusa <i@kazusa.me>
2024-09-27 15:39:45 +0800

    Fri Sep 27 03:39:45 PM CST 2024

--- 70.climbing-stairs.cpp
+++ 70.climbing-stairs.cpp
@@ -13,10 +13,11 @@ using namespace std;
 class Solution {
 public:
     int climbStairs(int n) {
-        vector<int> dp = {0, 1, 2};
-        while (dp.size() < n + 1) {
-            int i = dp.size();
-            dp.push_back(dp[i - 1] + dp[i - 2]);
+        vector<int> dp(max(n + 1, 3), 0);
+        dp[0] = -1; dp[1] = 1; dp[2] = 2;
+        if (n < 3) return dp[n];
+        for (int ix = 3; ix <= n; ++ix) {
+            dp[ix] = dp[ix - 1] + dp[ix - 2];
         }
         return dp[n];
     }
