d9b08a6e4dcf0a20b987d016c7eb407f0ec5ca28
kazusa <i@kazusa.me>
2024-09-27 15:30:33 +0800

    Fri Sep 27 03:30:33 PM CST 2024

--- 509.fibonacci-number.cpp
+++ 509.fibonacci-number.cpp
@@ -5,18 +5,19 @@
  */
 
 // @lc code=start
+
+#include <vector>
+
+using namespace std;
 class Solution {
 public:
     int fib(int n) {
-        int a = 0, b = 1;
-        int idx = 0;
-        while (idx != n) {
-            int t = a + b;
-            a = b;
-            b = t;
-            idx += 1;
+        vector<int> fibs(max(n + 1, 2), 0);
+        fibs[0] = 0; fibs[1] = 1;
+        for (int ix = 2; ix <= n; ++ix) {
+            fibs[ix] = fibs[ix - 1] + fibs[ix - 2];
         }
-        return a;
+        return fibs[n];
     }
 };
 // @lc code=end
