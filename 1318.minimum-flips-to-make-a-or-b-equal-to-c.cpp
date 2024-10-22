698122cb61943ec492d42b4505bec47edf89279b
kazusa <i@kazusa.me>
2024-10-22 12:51:59 +0800

    Tue Oct 22 12:51:59 PM CST 2024

--- 1318.minimum-flips-to-make-a-or-b-equal-to-c.cpp
+++ 1318.minimum-flips-to-make-a-or-b-equal-to-c.cpp
@@ -13,19 +13,10 @@ using namespace std;
 class Solution {
 public:
     int minFlips(int a, int b, int c) {
-        constexpr unsigned bits = 32;
-        bitset<bits> bsa (a), bsb (b), bsc (c);
-        unsigned cnt = 0;
-        for (unsigned ix = 0; ix != bits; ++ix) {
-            if (bsc[ix] == 0) {
-                if (bsa[ix] == 1) cnt += 1;
-                if (bsb[ix] == 1) cnt += 1;
-            }
-            if (bsc[ix] == 1) {
-                if (bsa[ix] == 0 && bsb[ix] == 0) cnt += 1;
-            }
-        }
-        return cnt;
+        constexpr unsigned num_bits = 32;
+        // return ((a | b) ^ c) + (a & b & ((a | b) ^ c));
+        return bitset<num_bits> ((a | b) ^ c).count()
+            + bitset<num_bits> (a & b & (~c)).count();
     }
 };
 // @lc code=end
