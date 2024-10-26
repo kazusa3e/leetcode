ac230874953bbb2b6d784c74476530f028d9c6ca
kazusa <i@kazusa.me>
2024-10-26 18:49:20 +0800

    Sat Oct 26 06:49:20 PM CST 2024

--- 912.sort-an-array.cpp
+++ 912.sort-an-array.cpp
@@ -6,15 +6,39 @@
 
 // @lc code=start
 
-#include <vector>
 #include <algorithm>
+#include <vector>
+#include <iostream>
 
 using namespace std;
+
+template <typename Iterator,
+    typename Comparator = std::less<typename std::iterator_traits<Iterator>::value_type>,
+    typename = typename std::enable_if_t<
+        std::is_convertible_v<decltype(std::declval<Comparator>()(
+            std::declval<typename std::iterator_traits<Iterator>::value_type>(),
+            std::declval<typename std::iterator_traits<Iterator>::value_type>()
+        )), bool>
+    >
+>
+void quick_sort(Iterator begin, Iterator end,
+    Comparator comp = Comparator()) {
+    if (begin >= end) return;
+    std::swap(*begin, *(begin + rand() % (end - begin)));
+    auto pivot = *begin; auto left = begin + 1, right = end - 1;
+    while (left <= right) {
+        while (left <= right && comp(*left, pivot)) ++left;
+        while (left <= right && !comp(*right, pivot)) --right;
+        if (left < right) std::swap(*left++, *right--);
+    }
+    std::swap(*begin, *right);
+    quick_sort(begin, right, comp); quick_sort(right + 1, end, comp);
+}
 
 class Solution {
 public:
     vector<int> sortArray(vector<int>& nums) {
-        std::sort(nums.begin(), nums.end());
+        quick_sort(nums.begin(), nums.end());
         return nums;
     }
 };
