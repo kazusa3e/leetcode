f5b994d63d413049abbee82928248fb0dd2806cc
kazusa <i@kazusa.me>
2024-10-30 18:59:50 +0800

    Wed Oct 30 06:59:50 PM CST 2024

--- 1268.search-suggestions-system.cpp
+++ 1268.search-suggestions-system.cpp
@@ -8,62 +8,33 @@
 
 #include <vector>
 #include <string>
-#include <memory>
-#include <array>
+#include <set>
 #include <algorithm>
-#include <queue>
-#include <stack>
 
 using namespace std;
+
 class Solution {
 private:
-    struct trie_node {
-        array<unique_ptr<trie_node>, 26> child;
-        bool is_end { false };
-    };
-private:
-    unique_ptr<trie_node> root { make_unique<trie_node>() };
-
-private:
-    void add(const string &s) {
-        auto iter = root.get();
-        for (const auto ch : s) {
-            if (iter->child[ch - 'a'] == nullptr) {
-                iter->child[ch - 'a'] = make_unique<trie_node>();
-            }
-            iter = iter->child[ch - 'a'].get();
-        }
-        iter->is_end = true;
-    }
-
-    void traverse(const trie_node *iter, string &prefix, vector<string> &&ret) const {
-        if (ret.size() == 3) return;
-        if (iter->is_end) ret.push_back(prefix);
-
-        for (unsigned ix = 0; ix != 26; ++ix) {
-            if (iter->child[ix] != nullptr) {
-                prefix += (char) ('a' + ix);
-                traverse(iter->child[ix].get(), prefix, std::move(ret));
-                prefix.pop_back();
-            }
-        }
-    }
-
+    set<string> s;
 
 public:
     vector<vector<string>> suggestedProducts(vector<string>& products, string searchWord) {
-        for (const auto &s : products) add(s);
-        vector<vector<string>> ret;
-        string prefix; auto iter = root.get();
+        for (const auto &w : products) s.insert(w);
+
+        vector<vector<string>> results;
+        string prefix;
         for (unsigned ix = 0; ix != searchWord.size(); ++ix) {
-            if (iter == nullptr) { ret.push_back({}); continue; }
-            prefix += searchWord[ix]; iter = iter->child[searchWord[ix] - 'a'].get();
-            if (iter == nullptr) { ret.push_back({}); continue; }
-            vector<string> curr;
-            traverse(iter, prefix, std::move(curr));
-            ret.push_back(std::move(curr));
+            prefix += searchWord[ix];
+            auto pos = s.lower_bound(prefix);
+            vector<string> level;
+            for (unsigned ix = 0; ix != 3; ++ix) {
+                if (pos != s.end() && !pos->compare(0, prefix.size(), prefix)) {
+                    level.push_back(*pos); ++pos;
+                }
+            }
+            results.push_back(level);
         }
-        return ret;
+        return results;
     }
 };
 // @lc code=end
