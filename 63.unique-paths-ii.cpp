8ff4001253ee53edb006e5bcba1f58f48c4f00ca
kazusa <i@kazusa.me>
2024-11-06 18:45:25 +0800

    Wed Nov  6 06:45:25 PM CST 2024

--- 63.unique-paths-ii.cpp
+++ 63.unique-paths-ii.cpp
@@ -13,28 +13,23 @@ using namespace std;
 class Solution {
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        if (obstacleGrid[0][0] == 1) return 0;
         int m = obstacleGrid.size(), n = obstacleGrid[0].size();
-        vector<vector<int>> dp(m, vector<int>(n, 0));
-        dp[0][0] = (obstacleGrid[0][0] == 1) ? 0 : 1;
-        for (int i = 0; i != m; ++i) {
-            for (int j = 0; j != n; ++j) {
+        vector<vector<unsigned>> dp(m, vector<unsigned>(n, 0)); dp[0][0] = 1;
+        for (unsigned i = 0; i != m; ++i) {
+            for (unsigned j = 0; j != n; ++j) {
                 if (i == 0 && j == 0) continue;
-                if (i == 0) {
-                    dp[i][j] = (obstacleGrid[i][j] == 1) ? 0 : dp[i][j - 1];
-                } else if (j == 0) {
-                    dp[i][j] = (obstacleGrid[i][j] == 1) ? 0 : dp[i - 1][j];
-                } else {
-                    if (obstacleGrid[i][j] == 1) {
-                        dp[i][j] = 0;
-                        continue;
-                    }
-                    int l = (obstacleGrid[i - 1][j] == 1) ? 0 : dp[i - 1][j];
-                    int u = (obstacleGrid[i][j - 1] == 1) ? 0 : dp[i][j - 1];
-                    dp[i][j] = l + u;
+                if (obstacleGrid[i][j] == 1) {
+                    dp[i][j] = 0; continue;
                 }
+                unsigned u = (i == 0) ? 0 :
+                    ((obstacleGrid[i - 1][j] == 1) ? 0 : dp[i - 1][j]);
+                unsigned l = (j == 0) ? 0 :
+                    ((obstacleGrid[i][j - 1] == 1) ? 0 : dp[i][j - 1]);
+                dp[i][j] = u + l;
             }
         }
-        return dp[m - 1][n - 1];
+        return dp.back().back();
     }
 };
 // @lc code=end
