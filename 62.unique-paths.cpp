b00b155ac082b65dec608af98865be3e7b6f70da
kazusa <i@kazusa.me>
2024-11-06 18:34:23 +0800

    Wed Nov  6 06:34:23 PM CST 2024

--- 62.unique-paths.cpp
+++ 62.unique-paths.cpp
@@ -12,20 +12,18 @@ using namespace std;
 
 class Solution {
 public:
+
+    long long combine(long long t, long long s) const {
+        if (s > t - s) s = t - s;
+        long long ret = 1;
+        for (long long i = 1; i <= s; ++i) {
+            ret *= t - s + i; ret /= i;
+        }
+        return ret;
+    }
+
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m, vector<int>(n, 0));
-        for (int i = 0; i != m; ++i) {
-            dp[i][0] = 1;
-        }
-        for (int j = 0; j != n; ++j) {
-            dp[0][j] = 1;
-        }
-        for (int i = 1; i != m; ++i) {
-            for (int j = 1; j != n; ++j) {
-                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
-            }
-        }
-        return dp[m - 1][n - 1];
+        return combine(m + n - 2, n - 1);
     }
 };
 // @lc code=end
