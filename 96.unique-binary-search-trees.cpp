76efdb1fe6f9af64f5d74cad54a658110a48d808
kazusa <i@kazusa.me>
2024-11-09 12:02:31 +0800

    Sat Nov  9 12:02:31 PM CST 2024

--- 96.unique-binary-search-trees.cpp
+++ 96.unique-binary-search-trees.cpp
@@ -13,12 +13,10 @@ using namespace std;
 class Solution {
 public:
     int numTrees(int n) {
-        vector<int> dp(20);
-        dp[0] = 1;
-        dp[1] = 1;
-        for (int i = 2; i <= 19; ++i) {
-            for (int j = 0; j != i; ++j) {
-                dp[i] += dp[j] * dp[i - j - 1];
+        vector<int> dp(max(n + 1, 3), 0); dp[0] = 1; dp[1] = 1; dp[2] = 2;
+        for (int ix = 3; ix != max(n + 1, 3); ++ix) {
+            for (int l = 0; l != ix; ++l) {
+                dp[ix] += (dp[l] * dp[ix - l - 1]);
             }
         }
         return dp[n];
