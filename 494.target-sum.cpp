705c88630c218df634ce98618aefc7beaf2555e6
kazusa <i@kazusa.me>
2024-11-23 16:49:41 +0800

    Sat Nov 23 04:49:41 PM CST 2024

--- 494.target-sum.cpp
+++ 494.target-sum.cpp
@@ -8,33 +8,38 @@
 
 #include <vector>
 #include <numeric>
+#include <cmath>
 
 using namespace std;
 
 class Solution {
-private:
-    vector<int> nums_;
-    int target_;
-    int num_methods {0};
-    int subsum {0};
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
-        nums_ = nums;
         const int sum = accumulate(nums.begin(), nums.end(), 0);
+        if (abs(target) > sum) return 0;
         if ((sum + target) % 2 == 1) return 0;
-        target_ = (sum + target) / 2;
+        const int caps = (sum + target) / 2;
 
-        backtracking(nums_.begin());
-        return num_methods;
-    }
+        // [item, caps] => num_methods
+        vector<vector<unsigned>> dp(nums.size(), vector<unsigned>(caps + 1, 0));
 
-    void backtracking(vector<int>::iterator begin) {
-        if (subsum == target_) num_methods += 1;
-        for (auto iter = begin; iter != nums_.end(); ++iter) {
-            subsum += *iter;
-            backtracking(iter + 1);
-            subsum -= *iter;
+        dp[0][0] = 1;
+        if (nums[0] <= caps) dp[0][nums[0]] = 1;
+        unsigned num_zeros = 0;
+        for (unsigned i = 0; i != nums.size(); ++i) {
+            if (nums[i] == 0) num_zeros += 1;
+            dp[i][0] = pow(2, num_zeros);
         }
+
+        for (unsigned i = 1; i != nums.size(); ++i) {
+            for (unsigned j = 0; j != caps + 1; ++j) {
+                dp[i][j] = dp[i - 1][j];
+                if (j >= nums[i]) {
+                    dp[i][j] += dp[i - 1][j - nums[i]];
+                }
+            }
+        }
+        return dp.back().back();
     }
 };
 // @lc code=end
