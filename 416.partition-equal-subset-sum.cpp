c5413b324e88d80f52d07282b969e039b9e65dac
kazusa <i@kazusa.me>
2024-11-23 15:46:25 +0800

    Sat Nov 23 03:46:25 PM CST 2024

--- 416.partition-equal-subset-sum.cpp
+++ 416.partition-equal-subset-sum.cpp
@@ -16,15 +16,15 @@ class Solution {
 public:
     bool canPartition(vector<int>& nums) {
         int sum = accumulate(nums.begin(), nums.end(), 0);
+        if (sum % 2 == 1) return false;
+
         vector<unsigned> dp(sum / 2 + 1, 0);
-        for (unsigned j = 0; j <= sum / 2; ++j) dp[j] = (j >= nums[0]) ? nums[0] : 0;
-        for (unsigned i = 1; i != nums.size(); ++i) {
+        for (unsigned i = 0; i != nums.size(); ++i) {
             for (unsigned j = sum / 2; j >= nums[i]; --j) {
-                if (j < nums[i]) dp[j] = dp[j];
-                else dp[j] = max(dp[j], dp[j - nums[i]] + nums[i]);
+                dp[j] = max(dp[j], dp[j - nums[i]] + nums[i]);
             }
         }
-        return dp.back() == (double) sum / 2;
+        return dp.back() == sum / 2;
     }
 };
 // @lc code=end
