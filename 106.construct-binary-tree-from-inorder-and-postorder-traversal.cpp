ca8e519ebe86c9c700cfead1ec49b9b088d00293
kazusa <i@kazusa.me>
2025-03-10 17:48:27 +0800

    Mon Mar 10 17:48:27 CST 2025

--- 106.construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 106.construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -5,6 +5,7 @@
  */
 
 // @lc code=start
+
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -16,7 +17,6 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-
 #include <algorithm>
 #include <vector>
 
@@ -24,17 +24,34 @@ using namespace std;
 
 class Solution {
 public:
+
+    using iterator = vector<int>::const_iterator;
+
     TreeNode *buildTree(vector<int> &inorder, vector<int> &postorder) {
-        if (postorder.empty()) return nullptr;
-        TreeNode *root = new TreeNode{postorder.back()};
-        if (postorder.size() == 1) return root;
-        auto pos = find(inorder.begin(), inorder.end(), postorder.back());
-        vector<int> inorder_leftpart = {inorder.begin(), pos},
-                    inorder_rightpart = {pos + 1, inorder.end()};
-        vector<int> postorder_leftpart = {postorder.begin(), postorder.begin() + inorder_leftpart.size()},
-                    postorder_rightpart = {postorder.begin() + inorder_leftpart.size(), postorder.begin() + inorder_leftpart.size() + inorder_rightpart.size()};
-        root->left = buildTree(inorder_leftpart, postorder_leftpart);
-        root->right = buildTree(inorder_rightpart, postorder_rightpart);
+        if (inorder.empty()) return nullptr;
+        return buildTree(inorder.cbegin(), inorder.cend(), postorder.cbegin(), postorder.cend());
+    }
+
+    TreeNode *buildTree(
+        iterator inorder_begin, iterator inorder_end,
+        iterator postorder_begin, iterator postorder_end) {
+        auto root = new TreeNode { *(postorder_end - 1) };
+        if (distance(postorder_begin, postorder_end) == 1) return root;
+
+        auto pos = find(inorder_begin, inorder_end, *(postorder_end - 1));
+        auto left_cnt = distance(inorder_begin, pos);
+
+        auto left_inorder_begin = inorder_begin, left_inorder_end = pos,
+            right_inorder_begin = pos + 1, right_inorder_end = inorder_end;
+        auto left_postorder_begin = postorder_begin, left_postorder_end = left_postorder_begin + left_cnt,
+            right_postorder_begin = left_postorder_end, right_postorder_end = postorder_end - 1;
+
+        if (distance(left_inorder_begin, left_inorder_end) != 0) {
+            root->left = buildTree(left_inorder_begin, left_inorder_end, left_postorder_begin, left_postorder_end);
+        }
+        if (distance(right_inorder_begin, right_inorder_end) != 0) {
+            root->right = buildTree(right_inorder_begin, right_inorder_end, right_postorder_begin, right_postorder_end);
+        }
         return root;
     }
 };
