28196a6c9cdd4ef1d0b279947ad88e7b83788229
kazusa <i@kazusa.me>
2025-03-11 10:15:51 +0800

    Tue Mar 11 10:15:51 AM CST 2025

--- 1470.shuffle-the-array.cpp
+++ 1470.shuffle-the-array.cpp
@@ -12,11 +12,14 @@ using namespace std;
 class Solution {
 public:
     vector<int> shuffle(vector<int>& nums, int n) {
-        vector<int> ret(n * 2); auto iter = ret.begin();
+        auto iter = nums.begin();
         for (auto i = nums.begin(), j = nums.begin() + n; i != nums.begin() + n; ++i, ++j) {
-            *(iter++) = *i; *(iter++) = *j;
+            *(iter++) |= (*i << 16); *(iter++) |= (*j << 16);
         }
-        return ret;
+        for (auto &el : nums) {
+            el = (el >> 16) & 0x0000ffff;
+        }
+        return nums;
     }
 };
 // @lc code=end
