41cfe8749e59af29f71c9ef43b94cd627b418562
kazusa <i@kazusa.me>
2025-03-11 09:55:46 +0800

    Tue Mar 11 09:55:46 AM CST 2025

--- 1346.check-if-n-and-its-double-exist.cpp
+++ 1346.check-if-n-and-its-double-exist.cpp
@@ -6,18 +6,20 @@
 
 // @lc code=start
 #include <vector>
-#include <set>
+#include <algorithm>
 
 using namespace std;
 
 class Solution {
 public:
     bool checkIfExist(vector<int>& arr) {
-        set<int> seen;
-        for (const auto &el : arr) {
-            if (auto pos = seen.find(el); pos != seen.end()) return true;
-            seen.insert(el * 2);
-            if (el % 2 == 0) seen.insert(el / 2);
+        sort(arr.begin(), arr.end());
+        for (auto iter = arr.begin(); iter != arr.end(); ++iter) {
+            auto pos = (*iter >= 0)
+                ? lower_bound(iter + 1, arr.end(), *iter * 2)
+                : lower_bound(arr.begin(), iter, *iter * 2);
+            if (pos == arr.end()) continue;
+            if (*iter * 2 == *pos && iter != pos) return true;
         }
         return false;
     }
